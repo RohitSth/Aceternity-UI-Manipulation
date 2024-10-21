18f535dd5a137b4eb6236ca465de90f5d5131002
RohitSth <shrestha.rohit655@gmail.com>
2024-10-21 10:37:32 +0545

    update :: merged lens and card hover effect

--- src/components/layouts/console/BottomSection.tsx
+++ src/components/layouts/console/BottomSection.tsx
@@ -1,39 +1,15 @@
-import Image from "next/image";
+"use client";
+
+import { HoverEffect } from "@/components/ui/card/card-hover-effect";
 
 interface BottomSectionProps {
-  image: string;
-  title: string;
+  items: { title: string; image: string; description: string; link: string }[];
 }
 
-export default function BottomSection({ image, title }: BottomSectionProps) {
+export default function BottomSection({ items }: BottomSectionProps) {
   return (
-    <div className="relative overflow-hidden rounded-lg bg-gray-800">
-      <div className="w-full h-[180px] relative overflow-hidden">
-        <div className="absolute top-2 left-2 bg-gray-900/80 px-2 py-1 z-20 rounded text-xs text-white flex items-center gap-1">
-          <svg
-            className="w-3 h-3"
-            fill="none"
-            strokeWidth="2"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-            />
-          </svg>
-          Your News
-        </div>
-        <Image
-          src={image || "/api/placeholder/400/250"}
-          alt={title}
-          className="object-cover"
-          fill
-          unoptimized
-          loading="lazy"
-        />
-      </div>
+    <div className="w-full relative">
+      <HoverEffect items={items} />
     </div>
   );
 }
